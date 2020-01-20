52118fb564d52d4baa3f198cf0a14b367010a342
doublejosh <joshuaklind@gmail.com>
2020-01-19 17:09:50 -0800

    Updates

---
+++ precache-manifest.28421c43ec7ce770aa46257da9355966.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b9ffea1c854f89d4f8dd63e3ffd189a",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d71e361493141674af5d",
+    "url": "/react-portfolio/static/css/2.58d385f6.chunk.css"
+  },
+  {
+    "revision": "22bb1d58c4e5e1e16130",
+    "url": "/react-portfolio/static/css/main.6fa8d653.chunk.css"
+  },
+  {
+    "revision": "d71e361493141674af5d",
+    "url": "/react-portfolio/static/js/2.ed95e66c.chunk.js"
+  },
+  {
+    "revision": "a8cd58e5c923ad16bd9d11fb82c48ff4",
+    "url": "/react-portfolio/static/js/2.ed95e66c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22bb1d58c4e5e1e16130",
+    "url": "/react-portfolio/static/js/main.a14a7d88.chunk.js"
+  },
+  {
+    "revision": "9f289a7f9c113993272b",
+    "url": "/react-portfolio/static/js/runtime-main.f68edf73.js"
+  },
+  {
+    "revision": "851ac5cd41c1272715245e19eb765cce",
+    "url": "/react-portfolio/static/media/aguaDeLuz.851ac5cd.png"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/react-portfolio/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/react-portfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/react-portfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/react-portfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/react-portfolio/static/media/ionicons.621bd386.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a3593377cf7411ce76680592d8503fcb.js"
+  "/react-portfolio/precache-manifest.28421c43ec7ce770aa46257da9355966.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
